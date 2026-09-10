218ce46ed92f180c82208e424b590fa4e86efeff
Gabriel Batista <gabriel027-alt@users.noreply.github.com>
2026-09-10 10:45:28 -0300

    fix: corrige hifen em object-contain]

--- src/components/AboutDoctor.tsx
+++ src/components/AboutDoctor.tsx
@@ -34,7 +34,7 @@ export default function AboutDoctor() {
       src="/images/dr-adher.jpg"
       alt="Dr. Adher Leonardo Leite Moura"
       fill
-      className="object-cover"
+      className="object-contain"
       priority
     />
   </div>
